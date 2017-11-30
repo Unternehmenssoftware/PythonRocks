601df3e27b3a61058c50d271d8d68227665db507
AntonSchaarschmidt <33436702+AntonSchaarschmidt@users.noreply.github.com>
2017-11-30 21:44:58 +0100

    Update scrapSchools_JSON.py

--- scrapSchools_JSON.py
+++ scrapSchools_JSON.py
@@ -11,26 +11,33 @@ del data[-1]
 #filter out the id,adress,school_type_entity
 schools = []
 for art in data:
-    schools.append([art['id'],art['address'],art['school_type_entity']])
+    schools.append([art['school_type'],art['school_type_entity'],art['name'],art['address']])
 
 #schools in Dataframe
-df = pd.DataFrame(schools, columns=['ID','Address','School_type'])
+df = pd.DataFrame(schools, columns=['Category','SubCategory','Name','Adress',])
 
-#split address to PLZ
-df['PLZ'] = df.Address.str.split(', ').str.get(1)
+#split adress ,add PLZ and remove plz out of adress
+df['ZipCode'] = df.Adress.str.split(', ').str.get(1)
+df['Adress'] = df.Adress.str.split(', ').str.get(0)
 
 #remove 'Berlin'
-df['PLZ'] = df['PLZ'].str.rstrip(' Berlin')
+df['ZipCode'] = df['ZipCode'].str.rstrip(' Berlin')
 
 #delete the unknown data
-df = df[df.PLZ != 'undefined']
+df = df[df.ZipCode != 'undefined']
 
 #drop column Address
-df = df.drop('Address', 1)
+#df = df.drop('Address', 1)
+
+#dataframe ordnen
+df = df[['ZipCode', 'Category', 'SubCategory', 'Name', 'Adress']]
 
 #save in Excel
 writer = pd.ExcelWriter('scrabSchools_JSON.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Schools', index=False)
 worksheet = writer.sheets['Schools']
 worksheet.set_column('B:B', 35)
+worksheet.set_column('C:C', 35)
+worksheet.set_column('D:D', 65)
+worksheet.set_column('E:E', 30)
 writer.save()
